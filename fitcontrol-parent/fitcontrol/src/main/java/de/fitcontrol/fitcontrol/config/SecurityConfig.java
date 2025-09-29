4094d97f51756daea9d490c7da1dbb892efbf586
Ines Drebenstedt <ines.drebenstedt@twinbait.de>
2025-09-29 09:56:17 +0200

    moved ci .properties

--- fitcontrol-parent/fitcontrol/src/main/java/de/fitcontrol/fitcontrol/config/SecurityConfig.java
+++ fitcontrol-parent/fitcontrol/src/main/java/de/fitcontrol/fitcontrol/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
+@Profile("!ci")
 @EnableWebSecurity
 public class SecurityConfig {
 	
